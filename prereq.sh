7dd87b7ad804d94b51cfe116d7e02121d3ca8ecb
cyb3rdoc <mastermind1024@gmail.com>
2025-07-05 12:19:13 +0400

    Update prereq.sh - python3-distutils deprecated

--- prereq.sh
+++ prereq.sh
@@ -2,5 +2,5 @@
 
 sudo apt update
 sudo apt install build-essential clang flex g++ gawk gcc-multilib gettext \
-    git libncurses5-dev libssl-dev python3-distutils rsync unzip zlib1g-dev \
+    git libncurses5-dev libssl-dev python3-setuptools rsync unzip zlib1g-dev \
     file wget zstd
